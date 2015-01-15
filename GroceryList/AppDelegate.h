9bdab15cfb830cd45c45ce6f80878f06c4bd7be9
Dylan Porter <dporter042@gmail.com>
2015-01-14 23:17:16 -0600

    Initial Commit

---
+++ GroceryList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroceryList
+//
+//  Created by Dylan Porter on 1/14/15.
+//  Copyright (c) 2015 Dylan Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GroceryList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroceryList
+//
+//  Created by Dylan Porter on 1/14/15.
+//  Copyright (c) 2015 Dylan Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
